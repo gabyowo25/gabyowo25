b5d3354de9eb6c94e3efef4ce1cd565d53700c1f
Gabriela Ayelen Martínez <104035944+gabyowo25@users.noreply.github.com>
2026-02-13 18:02:19 -0300

    Subo mi portfolio
    mi perfil

---
+++ script.js
@@ -0,0 +1,11 @@
+// Portfolio en construcción 😄
+// Acá después podés agregar animaciones o efectos.
+
+console.log("Portfolio de Gabriela cargado correctamente ✅");
+
+// Año automático en el footer
+const year = document.getElementById("year");
+year.textContent = new Date().getFullYear();
+
+const year = document.getElementById("year");
+year.textContent = new Date().getFullYear();
